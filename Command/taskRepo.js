1611f9f1e2143eeb27ea76966b03599690ca1406
ferdous-al-quraeshi <ecopavel@gmail.com>
2018-01-26 18:33:07 +0600

    building out repo a bit to realize the use case of this pattern

--- Command/taskRepo.js
+++ Command/taskRepo.js
@@ -1,4 +1,11 @@
 var repo = {
+    /* Building the pattern out a little bit
+        - start by adding an actual repo of tasks
+            - an empty 'tasks' object
+            - an empty 'commands' array 
+    */
+    tasks: {},
+    commands: [],
     get: function (id) {
         console.log('Getting task ' + id);
         return {
@@ -6,6 +13,8 @@ var repo = {
         };
     },
     save: function (task) {
+        /* save every 'task' under a 'task id' back into our repo */
+        repo.tasks[task.id] = task;
         console.log('Saving ' + task.name + ' to the db');
     }
 };
@@ -13,6 +22,13 @@ var repo = {
 /* Encapsulating the calling of any function from the 'repo' object as an individual entity */
 repo.execute = function (name) {
     var args = Array.prototype.slice.call(arguments, 1);
+    
+    /* drop a 'push' (the name of the command and whatever it is we passed in) into the 'commands' array */
+    repo.commands.push({
+        name: name,
+        obj: args[0]
+    });
+
     if(repo[name]) {
         return repo[name].apply(repo, args);
     }
@@ -24,3 +40,25 @@ repo.execute('save', {
     name: 'Task 1',
     completed: false
 });
+
+/* building the scenerio a little bit more...
+    - instead on one, we're gonna call 'execute' four times
+    - for more tasks with different id's
+*/
+repo.execute('save', {
+    id: 2,
+    name: 'Task 2',
+    completed: false
+});
+repo.execute('save', {
+    id: 3,
+    name: 'Task 3',
+    completed: false
+});
+repo.execute('save', {
+    id: 4,
+    name: 'Task 4',
+    completed: false
+});
+
+console.log(repo.tasks);
